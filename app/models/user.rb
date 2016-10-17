020637a579eec76456ae2ecc1cd9f02efc14ee68
Khanh Mata <radnvk@gmail.com>
2016-10-17 17:10:05 +0700

    Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
   has_many :exams
   has_many :suggest_questions
+
+  enum role: {user: 1, admin: 0}
+
 end
--- db/migrate/20161017123230_create_users.rb
+++ db/migrate/20161017123230_create_users.rb
@@ -2,11 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
       t.string :chatwork_id
-      t.string :password_digest
-      t.string :remember_digest
-      t.integer :role
+      t.integer :role, default: 1
       t.string :avatar
 
       t.timestamps null:false
